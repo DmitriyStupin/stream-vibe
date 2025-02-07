70e3be8116419c03484f1b4ff7348a4316ae1df1
Dmitry Stupin <dm.stup@mail.ru>
2025-02-07 15:24:42 +0700

    The Logo component, the classNames library

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,9 +1,12 @@
+import Logo from '@/components/Logo';
 import './Header.scss'
 
 const Header = () => {
   return (
     <header className='header'>
-      <div className="header__inner container"> </div>
+      <div className="header__inner container">
+        <Logo loading='eager'></Logo>
+      </div>
     </header>
   )
 }
