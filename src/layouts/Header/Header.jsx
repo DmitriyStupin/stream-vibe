ff5d857166016e6f0ab6420edc2745e94b27c21c
Dmitry Stupin <dm.stup@mail.ru>
2025-02-08 13:30:29 +0700

    Navigation menus, JSX lists, map array method

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,11 +1,51 @@
 import Logo from '@/components/Logo';
 import './Header.scss'
+import classNames from 'classnames';
 
-const Header = () => {
+const Header = (props) => {
+  const {
+    url,
+  } = props;
+
+  const menuItems = [
+    {
+      label: 'Home',
+      href: '/'
+    },
+    {
+      label: 'Movies & Shows',
+      href: '/movies'
+    },
+    {
+      label: 'Support',
+      href: '/support'
+    },
+    {
+      label: 'Subscriptions',
+      href: '/subscriptions'
+    }
+  ]
+
   return (
     <header className='header'>
       <div className="header__inner container">
         <Logo loading='eager'></Logo>
+        <nav className="header__menu">
+          <ul className="header__menu-list">
+            {menuItems.map(({label, href}, index) => (
+              <li className='header__menu-item' key={index}>
+                <a 
+                  href={href} 
+                  className={classNames('header__menu-link', {
+                    'is-active': href === url
+                  })}
+                >
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </header>
   )
