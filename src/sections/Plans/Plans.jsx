1ffe7d301270cdfbd65ad3b962b453695f6f998e
Dmitry Stupin <dm.stup@mail.ru>
2025-02-14 16:40:06 +0700

    Plans section — Tabs and TabsNavigation components

--- src/sections/Plans/Plans.jsx
+++ src/sections/Plans/Plans.jsx
@@ -2,26 +2,52 @@ import Grid from "@/components/Grid"
 import Section from "@/layouts/Section"
 import planGroups from "./planGroups"
 import PlanCard from "@/components/PlanCard"
+import TabsNavigation from "@/components/Tabs/components/TabsNavigation"
+import Tabs from "@/components/Tabs"
 
 const Plans = () => {
+  const tabsTitle = 'plans-tabs'
+  const tabsNavigationId = 'plans-tabs-navigation'
+
   return (
     <Section
       title="Choose the plan that's right for you"
       titleId="plans-id"
       description="Join StreamVibe and select from our flexible subscription options tailored to suit your viewing preferences. Get ready for non-stop entertainment!"
+      actions={(
+        <TabsNavigation
+          id={tabsNavigationId}
+          title={tabsTitle}
+          items={planGroups}
+        >
+        </TabsNavigation>
+      )}
     >
-      <Grid
-        columns={3}
-      >
-        {planGroups[0].items.map((planItem, index) => (
-          <PlanCard
-            {...planItem}
-            key={index}
-          >
 
-          </PlanCard>
-        ))}
-      </Grid>
+
+      <Tabs
+        title={tabsTitle}
+        navigationTargetElementId={tabsNavigationId}
+        items={planGroups.map((planGroup) => ({
+          title: planGroup.title,
+          isActive: planGroup.isActive,
+          children: (
+            <Grid
+              columns={3}
+            >
+              {planGroup.items.map((planItem, index) => (
+                <PlanCard
+                  {...planItem}
+                  key={index}
+                >
+
+                </PlanCard>
+              ))}
+            </Grid>
+          )
+        }))}
+      >
+      </Tabs>
     </Section>
   )
 }
