d7654f9634f84d253788c6ddd704c73af59c4b28
Dmitry Stupin <dm.stup@mail.ru>
2025-02-22 18:16:20 +0700

    The PlansComparison section is the Table component #2

--- src/sections/PlansComparison/PlansComparison.jsx
+++ src/sections/PlansComparison/PlansComparison.jsx
@@ -1,119 +1,133 @@
-import Section from '@/layouts/Section'
 import './PlansComparison.scss'
+import Section from '@/layouts/Section'
 import Badge from '@/components/Badge'
 import Table from '@/components/Table'
+import Specifications from '@/components/Specifications'
+import Tabs from '@/components/Tabs'
 
 const PlansComparison = () => {
   const headCells = [
     {
       children: 'Features',
-      width: '25%'
+      width: '25%',
     },
     {
       children: 'Basic',
-      width: '25%'
+      width: '25%',
+      tabsTitle: 'Basic',
     },
     {
-      children: <>Standart <Badge mode="accent">Popular</Badge> </>,
-      width: '25%'
+      children: <>Standard <Badge mode="accent">Popular</Badge></>,
+      width: '25%',
+      tabsTitle: 'Standart',
     },
     {
       children: 'Premium',
-      width: '25%'
+      width: '25%',
+      tabsTitle: 'Premium',
     },
   ]
 
   const rows = [
     {
-      cells: [ 
+      cells: [
         'Price',
         '$9.99/Month',
         '$12.99/Month',
         '$14.99/Month',
-      ]
+      ],
     },
-
     {
-      cells: [ 
+      cells: [
         'Content',
         'Access to a wide selection of movies and shows, including some new releases.',
         'Access to a wider selection of movies and shows, including most new releases and exclusive content',
         'Access to a widest selection of movies and shows, including all new releases and Offline Viewing',
-      ]
+      ],
+      isWide: true,
     },
-
     {
-      cells: [ 
+      cells: [
         'Devices',
         'Watch on one device simultaneously',
         'Watch on Two device simultaneously',
         'Watch on Four device simultaneously',
-      ]
+      ],
+      isWide: true,
     },
-
     {
-      cells: [ 
+      cells: [
         'Free Trail',
         '7 Days',
         '7 Days',
-        '7 Days',
-      ]
+        '7 Days,'
+      ],
     },
-
     {
-      cells: [ 
+      cells: [
         'Cancel Anytime',
         'Yes',
         'Yes',
         'Yes',
-      ]
+      ],
     },
-
     {
-      cells: [ 
+      cells: [
         'HDR',
         'No',
         'Yes',
-        'Yes',
-      ]
+        'Yes'
+      ],
     },
-
     {
-      cells: [ 
+      cells: [
         'Dolby Atmos',
         'No',
         'Yes',
-        'Yes',
-      ]
+        'Yes'
+      ],
     },
-
     {
-      cells: [ 
+      cells: [
         'Ad - Free',
         'No',
         'Yes',
-        'Yes',
-      ]
+        'Yes'
+      ],
     },
-
     {
-      cells: [ 
+      cells: [
         'Offline Viewing',
         'No',
         'Yes, for select titles.',
-        'Yes, for all titles.',
-      ]
+        'Yes, for all titles.'
+      ],
     },
-
     {
-      cells: [ 
+      cells: [
         'Family Sharing',
         'No',
         'Yes, up to 5 family members.',
-        'Yes, up to 6 family members.',
-      ]
+        'Yes, up to 6 family members.'
+      ],
     },
   ]
+
+  const tabsItems = headCells
+    .filter((headCell) => headCell.tabsTitle)
+    .map((headCell, headCellIndex) => ({
+      title: headCell.tabsTitle,
+      isActive: headCellIndex === 0,
+      children: (
+        <Specifications
+          items={rows.map(({ cells, isWide }) => ({
+            key: cells[0],
+            value: cells[headCellIndex + 1],
+            isWide,
+          }))}
+        />
+      )
+    }))
 
   return (
     <Section
@@ -122,11 +136,15 @@ const PlansComparison = () => {
       description="StreamVibe offers three different plans to fit your needs: Basic, Standard, and Premium. Compare the features of each plan and choose the one that's right for you."
     >
       <Table
+        className="hidden-mobile"
         headCells={headCells}
         rows={rows}
-      >
-
-      </Table>
+      />
+      <Tabs
+        className="visible-mobile"
+        title="plans-comparison-tabs-title"
+        items={tabsItems}
+      />
     </Section>
   )
 }
